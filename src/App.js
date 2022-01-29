d0ead7dbc36f1db1e695f0b2a9426ae46aa08c70
GAHYUN-P <gahyun94526@gmail.com>
2022-01-29 21:04:55 +0900

    클래스 컴포넌트로 네모뿌리기(2)

--- src/App.js
+++ src/App.js
@@ -12,15 +12,29 @@ class App extends React.Component {
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {}
+
+  addNemo = () => {
+    this.setState({count: this.state.count + 1});
 
   }
+
+  removeNemo = () => {
+    if(this.state.count > 0) { //갯수가 마이너스가 안되게하기위해
+    this.setState({count: this.state.count - 1});
+  } else {
+    window.alert("네모가 없어요")
+  }
+  }
+
+
   
   render(){
     const nemo_count = Array.from({length: this.state.count}, (v, i) => i);
     // length가 this.state.count만큼인 배열을 만들어주고 이 배열에 index값을 하나하나 넣어줄거야
 
     console.log(nemo_count)
+    console.log(this.state)
 
     return (
       <div className="App">
@@ -38,6 +52,11 @@ class App extends React.Component {
             </div>
           )
         })}
+
+        <div>
+          <button onClick={this.addNemo}>하나 추가</button>
+          <button onClick={this.removeNemo}>하나 빼기</button>
+        </div>
       </div>
     );
   }
